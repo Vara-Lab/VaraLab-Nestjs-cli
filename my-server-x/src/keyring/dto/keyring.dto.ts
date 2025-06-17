0b86912a235042347d9d34080b8a305694d1fd1b
DavidHM <dh29550@gmail.com>
2025-06-17 14:17:43 -0600

    package update

--- my-server-x/src/keyring/dto/keyring.dto.ts
+++
@@ -1,8 +0,0 @@
-import { HexString } from "@gear-js/api";
-
-export class KeyringData {
-    keyringAddress: HexString;
-    keyringVoucherId: HexString;
-    lockedKeyringData: any;
-    password: string
-}
